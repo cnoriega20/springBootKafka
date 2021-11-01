ba48ea4da82cab086a9a7c88cb663bd492ecbf89
cnoriega20 <c79.noriega@gmail.com>
2021-10-31 19:33:09 -0500

    Added lombok dependency and Employee Entity.

--- src/main/java/com/springboot/kafka/trn/model/Employee.java
+++ src/main/java/com/springboot/kafka/trn/model/Employee.java
@@ -1,4 +1,17 @@
 package com.springboot.kafka.trn.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private double salary;
 }
