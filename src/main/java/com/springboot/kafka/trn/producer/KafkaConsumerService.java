0e15255a9b63232ae97599ccb41e45d641c4de7e
cnoriega20 <c79.noriega@gmail.com>
2021-10-31 20:08:47 -0500

    Added Custom Serializer/Deserializer, lombok log and dependencies.

--- src/main/java/com/springboot/kafka/trn/producer/KafkaConsumerService.java
+++ src/main/java/com/springboot/kafka/trn/producer/KafkaConsumerService.java
@@ -1,19 +1,17 @@
 package com.springboot.kafka.trn.producer;
 
-import com.springboot.kafka.trn.consumer.KafkaProducerService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaConsumerService {
-    private static Logger logger =
-            LoggerFactory.getLogger(KafkaConsumerService.class);
+
 
     @KafkaListener(topics = "${springBootKafka.kafka.topic}", groupId ="kafka001" )
     public void consume(String message) {
-        logger.info("MESSAGE RECEIVED AT CONSUMER END -> " + message);
+        log.info("MESSAGE RECEIVED AT CONSUMER END -> " + message);
     }
 
 }
